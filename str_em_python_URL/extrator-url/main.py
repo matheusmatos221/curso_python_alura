78390ed9c019cb5d7fa3970d47033215702ebfea
Matheus Matos <matheus.matos@accurate.com.br>
2021-12-02 01:42:11 -0300

    2.Utilizando métodos de strings

--- str_em_python_URL/extrator-url/main.py
+++ str_em_python_URL/extrator-url/main.py
@@ -1,11 +1,27 @@
 # Fatiamento (slicing)
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
 
-url_base = url[0:19]
-print(url_base)
+# URL
+url = "https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
 
-url_parametros = url[20:36]
+# Separa a URL em URL Base e URL Parâmetros
+indice_interrogacao = url.find("?")
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
 print(url_parametros)
 
-print(url)
+# Indices do parâmetro de busca e do valor do parâmetro
+parametro_busca = "moedaDestino"
+indice_parametro_busca = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro_busca + len(parametro_busca) + 1
+
+# Left slice
+valor = url_parametros[indice_valor:]
+
+# Right slice
+if valor.find("&") == -1:
+    pass
+else:
+    valor = valor[:valor.find("&")]
+
+# Print valor
+print(f'O valor do parâmetro ¨{parametro_busca}¨ é: {valor}')
